8c6677053bc3948847fb388cb7666877283d5ea6
Chris Pollard <cspollard@gmail.com>
2015-12-12 21:04:08 +0000

    output to yoda!

--- categorizeEvents.hs
+++ categorizeEvents.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import qualified Data.ByteString.Lazy as BSL
@@ -8,10 +10,15 @@ import Data.HEP.Atlas.Jet
 import Data.HEP.Cut
 import Data.HEP.LorentzVector
 
-import Data.Histogram.Generic hiding (zip)
+import Data.Histogram.Generic hiding (zip, map)
 import Data.Histogram.Fill
 
+import Control.Monad (forM_)
+import Data.Text hiding (map, take)
+import Data.Monoid
+
 import qualified Data.Vector as V
+import Data.Uncertain
 
 import Data.Hep.Atlas.Histograms 
 
@@ -28,11 +35,25 @@ nBtags :: Event -> Int
 nBtags = nJets $ minPt 25000 `cAnd` maxAbsEta 2.5 `cAnd` minMV2c20 0.7
 
 
-toTuple :: IntervalBin b => Histogram V.Vector b a -> V.Vector ((BinValue b, BinValue b), a)
-toTuple h = V.zip (binsList . bins $ h) (histData h)
+toTuple :: IntervalBin b => Histogram V.Vector b a -> [((BinValue b, BinValue b), a)]
+toTuple h = V.toList $ V.zip (binsList . bins $ h) (histData h)
 
 main :: IO ()
 main = do
         evts <- decodeList `fmap` BSL.getContents :: IO Events
 
-        mapM_ print . fillBuilder allHists $ evts
+        let hists = fillBuilder allHists evts
+
+        forM_ hists $ \(t, hists') -> do
+            forM_ hists' $ \(s, h) -> do
+                let p = "/HTop/" <> t <> s
+                putStrLn . unpack $ "# BEGIN YODA_HISTO1D " <> p
+                putStrLn . unpack $ "Path=" <> p
+                putStrLn "Type=Histo1D"
+                putStrLn . unpack $ "XLabel=" <> s
+                putStrLn "Total\tTotal\t1.0\t1.0\t1.0\t1.0\t1"
+                putStrLn "Underflow\tUnderflow\t0.0\t0.0\t0.0\t0.0\t0"
+                putStrLn "Overflow\tOverflow\t0.0\t0.0\t0.0\t0.0\t0"
+                mapM_ putStrLn . map (\((xmin, xmax), U y dy) -> show xmin ++ "\t" ++ show xmax ++ "\t" ++ show y ++ "\t" ++ show (dy*dy) ++ "1.0\t1.0\t1") $ toTuple h
+                putStrLn "# END YODA_HISTO1D"
+                putStrLn ""
