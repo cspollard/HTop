64d0365b397c6d6f1a745d8648512e10dd859406
Chris Pollard <cspollard@gmail.com>
2016-02-02 18:18:31 +0000

    now compiles.

--- app/combineHist.hs
+++ app/combineHist.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import System.Environment (getArgs)
 import qualified Data.ByteString.Char8 as BSC
 import Data.HEP.Atlas.CrossSections
 import Data.Attoparsec.ByteString.Char8 (parseOnly)
@@ -10,3 +11,5 @@ main :: IO ()
 main = do
     fin <- head <$> getArgs
     xsecs <- parseOnly crossSectionInfo <$> BSC.readFile fin
+
+    print "THIS CURRENTLY DOES NOTHING."
