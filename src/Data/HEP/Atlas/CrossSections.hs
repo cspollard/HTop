dd6f97ae563c567bbfc74ffcf38002216844c158
Chris Pollard <cspollard@gmail.com>
2016-02-02 15:19:10 +0000

    allow for white space at the beginning of the cross section file

--- src/Data/HEP/Atlas/CrossSections.hs
+++ src/Data/HEP/Atlas/CrossSections.hs
@@ -15,7 +15,7 @@ isEndOfLine c = c == '\n' || c == '\r'
 type CrossSectionInfo = IntMap Double
 
 crossSectionInfo :: Parser CrossSectionInfo
-crossSectionInfo = fromList . rights <$> many (eitherP comment xsecline) <* endOfInput
+crossSectionInfo = fromList . rights <$> skipSpace *> many (eitherP comment xsecline) <* endOfInput
                     where
                         comment = char '#' *> takeTill isEndOfLine *> skipSpace
                         xsecline = (,) <$> (decimal <* skipSpace) <*> ((*) <$> (double <* skipSpace) <*> (option 1.0 (double <* skipSpace)))
