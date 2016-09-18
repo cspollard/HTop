9acb2f7690fd89ac80a57816116ace7413415401
Chris Pollard <cspollard@gmail.com>
2016-09-18 14:37:14 +0100

    just some code restructuring.

--- src/Data/Atlas/Jet.hs
+++ src/Data/Atlas/Jet.hs
@@ -19,17 +19,29 @@ import Data.TTree
 
 import qualified Data.Vector as V
 
-data Jet = Jet { jPtEtaPhiE :: PtEtaPhiE
-               , jMV2c10 :: Float
-               , jJVT :: Float
-               , jPVTracks :: [PtEtaPhiE]
-               , jSVTracks :: [PtEtaPhiE]
-               } deriving (Show, Generic)
+data Jet =
+    Jet
+        { jPtEtaPhiE :: PtEtaPhiE
+        , jMV2c10 :: Float
+        , jJVT :: Float
+        , jPVTracks :: [PtEtaPhiE]
+        , jSVTracks :: [PtEtaPhiE]
+        } deriving (Show, Generic)
 
 instance Serialize Jet
 
 instance HasLorentzVector Jet where
     toPtEtaPhiE = lens jPtEtaPhiE $ \j lv -> j { jPtEtaPhiE = lv }
+
+data JetTruthLabel =
+    BLabel
+    | CLabel
+    | TauLabel
+    | LLabel
+    | NoLabel
+
+instance FromTTree JetTruthLabel where
+    fromTTree = do l <- readBranch "
 
 newtype Jets = Jets { fromJets :: [Jet] } deriving (Show, Generic, Serialize)
 
@@ -38,20 +50,21 @@ jetTracksIsTight = V.toList . fmap V.toList .  over (traverse.traverse) (/= (0 :
 
 
 instance FromTTree Jets where
-    fromTTree = do PtEtaPhiEs tlvs <- lvsFromTTree "JetPt" "JetEta" "JetPhi" "JetE"
-                   mv2c10s <- readBranch "JetMV2c20"
-                   jvts <- readBranch "JetJVT"
-                   trks <- jetTracksTLV "JetTracksPt" "JetTracksEta" "JetTracksPhi" "JetTracksE"
-                   trksTight <- jetTracksIsTight
+    fromTTree = do
+        PtEtaPhiEs tlvs <- lvsFromTTree "JetPt" "JetEta" "JetPhi" "JetE"
+        mv2c10s <- readBranch "JetMV2c20"
+        jvts <- readBranch "JetJVT"
+        trks <- jetTracksTLV "JetTracksPt" "JetTracksEta" "JetTracksPhi" "JetTracksE"
+        trksTight <- jetTracksIsTight
 
-                   let trks' = fmap snd . filter fst <$> zipWith zip trksTight trks
+        let trks' = fmap snd . filter fst <$> zipWith zip trksTight trks
 
-                   sv1trks <- jetTracksTLV "JetSV1TracksPt" "JetSV1TracksEta" "JetSV1TracksPhi" "JetSV1TracksE"
+        sv1trks <- jetTracksTLV "JetSV1TracksPt" "JetSV1TracksEta" "JetSV1TracksPhi" "JetSV1TracksE"
 
-                   let trks'' = zipWith (deleteFirstsBy trkEq) trks' sv1trks
+        let trks'' = zipWith (deleteFirstsBy trkEq) trks' sv1trks
 
-                   let js = Jet <$> ZipList tlvs <*> mv2c10s <*> jvts <*> ZipList trks'' <*> ZipList sv1trks
-                   return . Jets $ getZipList js
+        let js = Jet <$> ZipList tlvs <*> mv2c10s <*> jvts <*> ZipList trks'' <*> ZipList sv1trks
+        return . Jets $ getZipList js
 
 
 pvTrkSumTLV :: Jet -> PtEtaPhiE
@@ -81,16 +94,18 @@ bFrag j = case trkSumPt j of
 
 jetTracksTLV :: MonadIO m
              => String -> String -> String -> String -> TR m [[PtEtaPhiE]]
-jetTracksTLV spt seta sphi se = do trkpts <- fromVVector <$> readBranch spt
-                                   trketas <- fromVVector <$> readBranch seta
-                                   trkphis <- fromVVector <$> readBranch sphi
-                                   trkes <- fromVVector <$> readBranch se
+jetTracksTLV spt seta sphi se = do
+    trkpts <- fromVVector <$> readBranch spt
+    trketas <- fromVVector <$> readBranch seta
+    trkphis <- fromVVector <$> readBranch sphi
+    trkes <- fromVVector <$> readBranch se
 
-                                   let trks = V.zipWith4 (\pts etas phis es ->
-                                                           V.toList $ V.zipWith4 PtEtaPhiE pts etas phis es
-                                                          ) trkpts trketas trkphis trkes
+    let trks = V.zipWith4
+            ( \pts etas phis es ->
+                V.toList $ V.zipWith4 PtEtaPhiE pts etas phis es
+            ) trkpts trketas trkphis trkes
 
-                                   return $ V.toList trks
+    return $ V.toList trks
 
 trkEq :: PtEtaPhiE -> PtEtaPhiE -> Bool
 p `trkEq` p' = (p `lvDR` p') < 0.005
