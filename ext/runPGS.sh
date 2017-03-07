dca65b3536a75c9cc55325703aed4272e812dc60
Chris Pollard <cspollard@gmail.com>
2017-03-07 11:24:05 +0000

    some changes

--- ext/runPGS.sh
+++ ext/runPGS.sh
@@ -2,8 +2,5 @@
 
 cd $PBS_O_WORKDIR
 source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
-localSetupROOT
-mkdir $1.run
-cd $1.run
-ls /nfs/atlas/chgray01/QualificationProject/NTuples_210916/user.chgray.$1*/*.root* > infiles
-run-htop --outfile hist.gz --infiles infiles --xsecfile ../data/XSection-MC15-13TeV-fromSusyGrp.data > log 2>&1
+lsetup root
+run-htop --outfile $1.hist.gz --infiles $1.infiles > $1.log 2>&1
