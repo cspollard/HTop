7521c9b055fa56afcceb49788b15ac5c73da5dc8
Chris <cspollard@gmail.com>
2017-03-17 10:42:01 +0000

    write timing info to PGS output

--- ext/runPGS.sh
+++ ext/runPGS.sh
@@ -3,4 +3,4 @@
 cd $PBS_O_WORKDIR
 source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
 lsetup root
-run-htop --outfile $1.hist.gz --infiles $1.infiles > $1.log 2>&1
+time run-htop --outfile $1.hist.gz --infiles $1.infiles > $1.log 2>&1
