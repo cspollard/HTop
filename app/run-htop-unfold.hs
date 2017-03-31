9fb8e357ee3fd2189257365b5e3820cbd327fc78
Chris Pollard <cspollard@gmail.com>
2017-03-31 14:50:43 +0100

    add poisson fluctuations.

--- app/run-htop-unfold.hs
+++ app/run-htop-unfold.hs
@@ -13,18 +13,18 @@ import           Atlas
 import           Atlas.ProcessInfo
 import           Atlas.ToYoda
 import           BFrag.Systematics
-import           Control.Applicative    (liftA2)
-import           Control.Arrow          (first, second)
+import           Control.Applicative           (liftA2)
+import           Control.Arrow                 (first, second)
 import           Control.Lens
-import qualified Data.Histogram.Generic as H
-import qualified Data.IntMap.Strict     as IM
-import qualified Data.Map.Strict        as M
-import qualified Data.Text              as T
-import qualified Data.Vector            as V
-import           Debug.Trace
+import qualified Data.Histogram.Generic        as H
+import qualified Data.IntMap.Strict            as IM
+import qualified Data.Map.Strict               as M
+import qualified Data.Text                     as T
+import qualified Data.Vector                   as V
 import           Model
 import           RunModel
-import           System.IO              (hFlush, stdout)
+import           System.IO                     (hFlush, stdout)
+import           System.Random.MWC.Probability
 
 
 type TextMap a = M.Map T.Text a
@@ -34,7 +34,6 @@ main = mainWith tmp
 
 tmp :: Double -> String -> ProcMap (Folder (Vars YodaObj)) -> IO ()
 tmp lu outfile procs = do
-  mapMOf_ (ix 410000.to (M.keys.folderToMap).traverse) print procs
   let procst =
         M.fromList
         . fmap (first processTitle . second folderToMap)
@@ -47,24 +46,25 @@ tmp lu outfile procs = do
       truehname = "/elmujj/truthjets/zbt"
       -- TODO
       -- partial!
-      ttbarrecoh = trace "ttbarrecoh" $ procst ^?! ix "Pow+Py (nominal)" . ix recohname
-      ttbartrueh = trace "ttbartrueh" $ procst ^?! ix "Pow+Py (nominal)" . ix truehname
+      ttbarrecoh = procst ^?! ix "Pow+Py (nominal)" . ix recohname
+      ttbartrueh = procst ^?! ix "Pow+Py (nominal)" . ix truehname
       ttbarmath = procst
         ^?! ix "Pow+Py (nominal)"
           . ix "/elmujj/probejets/4psvtrks/ptgt30/matched/recobfragvstruebfrag"
       bkgHs = (^?! ix recohname) <$> sans "Pow+Py (nominal)" procst
-      (dataH, model, params) =
-        buildModel
-          lu
-          ttbarrecoh
-          ttbartrueh
-          ttbarmath
-          bkgHs
-          -- TODO
-          -- the bottom is real data.
-          (over (noted._H1DD) (scaling 38000) $ ttbarrecoh ^. nominal)
-          -- (ttbarrecoh ^?! variations . ix "PowPyRadDown")
-          -- (procs ^?! ix 0 . to folderToMap . ix recohname . nominal)
+
+  (dataH, model, params) <-
+    buildModel
+      lu
+      ttbarrecoh
+      ttbartrueh
+      ttbarmath
+      bkgHs
+      -- TODO
+      -- the bottom is real data.
+      (over (noted._H1DD) (scaling 38000) $ ttbarrecoh ^. nominal)
+      -- (ttbarrecoh ^?! variations . ix "PowPyRadDown")
+      -- (procs ^?! ix 0 . to folderToMap . ix recohname . nominal)
 
   putStrLn "data:"
   print dataH
@@ -82,40 +82,26 @@ buildModel
   -> Vars YodaObj
   -> TextMap (Vars YodaObj)
   -> YodaObj
-  -> (V.Vector Int, Model Double, TextMap (ModelParam Double))
-buildModel lu recoH trueH matH bkgHs dataH =
-  let
-      -- TODO
-      -- TODO
-      -- currently cutting off last truth and reco bins!
-      vtrue = getH1DD <$> trueH
+  -> IO (V.Vector Int, Model Double, TextMap (ModelParam Double))
+buildModel lu recoH trueH matH bkgHs dataH = do
+  vdata <-
+    withSystemRandom . asGenIO . sample . traverse poisson $ getH1DD dataH
+
+  let vtrue = getH1DD <$> trueH
       vreco = getH1DD <$> recoH
-      vdata = floor <$> getH1DD dataH
       vvmat = getH2DD <$> matH
       vbkgs = fmap getH1DD <$> bkgHs
 
-      -- nreco = traceShowId . trace "nreco" $ views nominal length vreco'
-      -- vreco = V.slice 0 (nreco-1) <$> vreco'
-      -- vvmat' = fmap (V.slice 0 (nreco-1)) . getH2DD <$> matH
-      -- vdata = fmap (floor . (*lu)) . V.slice 0 (nreco-1) $ getH1DD dataH
-      -- vbkgs = fmap (V.slice 0 (nreco-1) . getH1DD) <$> bkgHs
-
-      -- vtrue = rebin 2 (+) <$> vtrue'
-      -- vvmat = rebin 2 (V.zipWith (+)) <$> vvmat'
-
       emptysig = const 0 <$> nom vtrue
 
-      signalreco =
-        traceShowId . trace "signalreco"
-        $ foldl (V.zipWith (+)) (const 0 <$> nom vreco) <$> vvmat
+      signalreco = foldl (V.zipWith (+)) (const 0 <$> nom vreco) <$> vvmat
 
       -- TODO
       -- TODO
       -- how are we getting negative numbers in the bkgs?
       -- aMC has negative weights...
       bkgreco =
-        traceShowId . trace "bkgreco"
-        . (fmap.fmap) (\x -> if x < 0 then 0 else x)
+        (fmap.fmap) (\x -> if x < 0 then 0 else x)
         $ V.zipWith (-) <$> vreco <*> signalreco
 
       mvbkgs = sequenceA $ vbkgs & at "ttbarlight" ?~ bkgreco
@@ -159,7 +145,7 @@ buildModel lu recoH trueH matH bkgHs dataH =
 
       params = M.union sysparams trueparams
 
-  in (vdata, nommod, params)
+  return (vdata, nommod, params)
 
   where
     -- TODO
