3f7fd0907e2f357ee2ecebf0b13f1ab195b10220
Chris <cspollard@gmail.com>
2017-09-13 08:04:07 +0100

    make sure correct root version is setup...

--- ext/runPGS.sh
+++ ext/runPGS.sh
@@ -4,6 +4,11 @@
 # for f in 0 1 2 3; do qsub -j oe -q medium6 -v F=run/$f ext/runPGS.sh; done
 
 cd $PBS_O_WORKDIR
+
+echo "files to be analyzed:"
+for f in `cat $F.infiles`; do ls $f; done
+
 source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
-lsetup root
+lsetup "root 6.04.14-x86_64-slc6-gcc49-opt"
+
 time run-htop --outfile $F.hist.gz --infiles $F.infiles > $F.log 2>&1
