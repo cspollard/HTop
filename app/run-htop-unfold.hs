2750c5a3ee7f06108702a240ca9127d739af97af
Chris Pollard <cspollard@gmail.com>
2017-10-08 17:35:31 +0200

    uncrry.

--- app/run-htop-unfold.hs
+++ app/run-htop-unfold.hs
@@ -124,7 +124,7 @@ main = do
               fmap $ \(x, y) ->
                 let x' = T.toLower x
                     s' = T.toLower s
-                    x'' = uncurry (<>) $ T.breakOn s' x'
+                    x'' = mconcat $ T.splitOn s' x'
                 in (x'', y)
 
             (downs, ups) =
