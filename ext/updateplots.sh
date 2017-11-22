11e14021a62cdba2c2dc11577c384dc6a4e01c02
Chris Pollard <cspollard@gmail.com>
2017-11-22 17:43:27 +0100

    plot structure

--- ext/updateplots.sh
+++ ext/updateplots.sh
@@ -7,4 +7,4 @@ rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.
 rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.yoda:"PowPy8" yoda/PowPy8RadUpAFII.yoda:"PowPy8RadUp" yoda/PowPy8RadDownAFII.yoda:"PowPy8RadDown" yoda/PowPy6FS.yoda:"PowPy6" -o closuredata/powpygenplots
 
 mkdir -p realdata
-rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda yoda/me.yoda yoda/rad.yoda yoda/puwgt.yoda yoda/background.yoda:"backgrounds" -o realdata/plots
+rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda yoda/me.yoda yoda/rad.yoda yoda/puwgt.yoda yoda/background.yoda:"backgrounds" -o realdata/recoplots
