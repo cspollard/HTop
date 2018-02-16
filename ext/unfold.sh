ab8c4cc471eb5b4485fd474c6827635d55b6df60
Chris <cspollard@gmail.com>
2018-02-16 12:59:02 +0000

    some unfolding updates.

--- ext/unfold.sh
+++ ext/unfold.sh
@@ -13,5 +13,5 @@ stack exec run-htop-unfold -- --mcmcfile mcmc/closure/zblcmcmc.dat --yodafolder 
 
 mkdir -p particlelevel/closure/unfoldzbtrelc
 mkdir -p particlelevel/data/unfoldzbtrelc
-stack exec run-htop-unfold -- --mcmcfile mcmc/data/zbtrelcmcmc.dat --yodafolder particlelevel/data/unfoldzbtrelc --xsecfile ext/XSection-MC15-13TeV.data --observable zbtrelc hist/*hist.gz > mcmc/data/zbtrelc.log 2>&1
+stack exec run-htop-unfold -- --mcmcfile mcmc/data/zbtrelcmcmc.dat --yodafolder particlelevel/data/unfoldzbtrelc --xsecfile ext/XSection-MC15-13TeV.data --observable zbtrelc hist/*hist.gz > mcmc/data/zbtrelc.log 2>&1 &
 stack exec run-htop-unfold -- --mcmcfile mcmc/closure/zbtrelcmcmc.dat --yodafolder particlelevel/closure/unfoldzbtrelc --xsecfile ext/XSection-MC15-13TeV.data --observable zbtrelc hist/{3,4}*hist.gz > mcmc/closure/zbtrelc.log 2>&1 &
