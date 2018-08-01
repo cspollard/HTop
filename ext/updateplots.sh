dae8968dc3bc76959ca4b41c66468996fd36483c
Chris Pollard <cspollard@gmail.com>
2018-08-01 11:42:58 +0200

    move to fsr variation

--- ext/updateplots.sh
+++ ext/updateplots.sh
@@ -19,4 +19,4 @@ rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.
 rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.yoda:"PowPy8" yoda/PowPy8RadUpAFII.yoda:"PowPy8RadUp" yoda/PowPy8RadDownAFII.yoda:"PowPy8RadDown" yoda/PowPy6FS.yoda:"PowPy6" -o particlelevel/powpygenplotsnodata
 
 mkdir -p detectorlevel
-rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda yoda/me.yoda yoda/rad.yoda yoda/puwgt.yoda yoda/background.yoda:"backgrounds" -o detectorlevel/plots
+rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda yoda/rad.yoda yoda/fsr.yoda yoda/puwgt.yoda yoda/background.yoda:"backgrounds" -o detectorlevel/plots
