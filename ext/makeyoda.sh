e7944b11c7e14c4b63f929a645fbc5cd70eb1e77
Chris Pollard <cspollard@gmail.com>
2018-08-14 10:00:19 +0200

    moved Xsec file

--- ext/makeyoda.sh
+++ ext/makeyoda.sh
@@ -1,2 +1,2 @@
 mkdir -p yoda
-stack exec run-htop-toyoda -- -o yoda --xsecfile ext/XSection-MC15-13TeV.data hist/*hist.gz
+stack exec run-htop-toyoda -- -o yoda --xsecfile data/XSection-MC15-13TeV.data hist/*hist.gz
