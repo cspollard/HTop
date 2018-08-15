c98466a9cabb8a8459a7ce2e01f479bea8a1e07f
Chris Pollard <cspollard@gmail.com>
2018-08-15 12:28:49 +0200

    fix up detector-level plots

--- ext/updateplots.sh
+++ ext/updateplots.sh
@@ -19,4 +19,5 @@ rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.
 rivet-mkhtml --mc-errs --pwd -m ".*/truejets/.*" -c ext/htop.plot yoda/PowPy8FS.yoda:"PowPy8" yoda/PowPy8FSRUpAFII.yoda:"PowPy8FSRUp" yoda/PowPy8FSRDownAFII.yoda:"PowPy8FSRDown" yoda/PowPy6FS.yoda:"PowPy6" -o particlelevel/powpygenplotsnodata
 
 mkdir -p detectorlevel
-rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda yoda/rad.yoda yoda/fsr.yoda yoda/puwgt.yoda yoda/background.yoda:"backgrounds" -o detectorlevel/plots
+rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/ps.yoda:"parton shower" yoda/rad.yoda:"ISR" yoda/fsr.yoda:"FSR" yoda/puwgt.yoda:"pileup" yoda/background.yoda:"backgrounds" -o detectorlevel/modeling
+rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/data.yoda yoda/nominal.yoda yoda/npvtrksf.yoda:"PV track multiplicity" yoda/nsvtrksf.yoda:"SV track multiplicity" yoda/v2trk_fake_rate_tight.yoda:"track fake rate" yoda/v2trk_eff_loose_global.yoda:"track efficiency" yoda/v2trk_res_d0_meas.yoda:"track \$d_0\$" yoda/v2trk_res_z0_meas.yoda:"track \$z_0\$" yoda/background.yoda:"backgrounds" -o detectorlevel/tracking
