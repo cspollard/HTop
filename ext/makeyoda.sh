ff68c4b75328d1a9a6c7ba5bd0488f4dd9d3a429
Chris Pollard <cspollard@gmail.com>
2019-12-06 10:40:37 +0100

    problem with ptc systematic

--- ext/makeyoda.sh
+++ ext/makeyoda.sh
@@ -1,5 +1,2 @@
 mkdir -p yoda
-stack exec run-htop-toyoda -- \
-  -o yoda \
-  --xsecfile data/XSection-MC15-13TeV.data hist/*hist.gz \
-  --negex "4psv" --negex "5psv"
+stack exec run-htop-toyoda -- -o yoda --xsecfile data/XSection-MC15-13TeV.data hist/*hist.gz
--- ext/plotreco.sh
+++ ext/plotreco.sh
@@ -1,5 +1,5 @@
 rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/total.yoda yoda/background.yoda:"backgrounds" yoda/data.yoda:"LineColor=black" -o detectorlevel/total
 
-rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/nominal.yoda yoda/ps.yoda:"parton shower" yoda/rad.yoda:"ISR" yoda/fsr.yoda:"FSR" yoda/puwgt.yoda:"pileup" yoda/background.yoda:"backgrounds" yoda/data.yoda:"LineColor=black" -o detectorlevel/modeling
+rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/nominal.yoda yoda/ps.yoda:"parton shower" yoda/fsr.yoda:"FSR" yoda/puwgt.yoda:"pileup" yoda/ptcsf.yoda:"jet $p_T^c$" yoda/background.yoda:"backgrounds" yoda/data.yoda:"LineColor=black" -o detectorlevel/modeling
 
 rivet-mkhtml -m ".*/elmujj/.*" --mc-errs --pwd -c ext/htop.plot yoda/nominal.yoda yoda/nsvtrksf.yoda:"SV track multiplicity" yoda/v2trk_fake_rate_tight.yoda:"track fake rate" yoda/v2trk_eff_tight_global.yoda:"track efficiency" yoda/v2trk_res_d0_meas.yoda:"track \$d_0\$" yoda/v2trk_res_z0_meas.yoda:"track \$z_0\$" yoda/background.yoda:"backgrounds" yoda/data.yoda:"LineColor=black" -o detectorlevel/tracking
