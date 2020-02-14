c96a5f57a57837390950883b0732c96aff31fe71
Chris Pollard <cspollard@gmail.com>
2020-02-14 15:42:52 +0100

    more cleanup of pvalue script

--- ext/pvalue.py
+++ ext/pvalue.py
@@ -1,3 +1,29 @@
+"""
+to run this script call
+
+$ python pvalue.py histogram infile
+
+where the "histogram" parameter indicates which histogram should be read in
+from the yoda files and "infile" is a suitable data file of toys.
+the input yoda files should be indicated by changing the "files" variable
+below.
+"""
+
+# please give a label and a file path for each yoda file for which you'd like to
+# calculate the pvalue.
+files = \
+    { "PowPy8" : "yoda.nochi2/PowPy8FS.yoda" \
+    , "Sherpa221" : "yoda.nochi2/Sherpa221AFII.yoda" \
+    , "Powheg+Herwig7": "yoda.nochi2/PowH7AFII.yoda" \
+    , "Powheg+Pythia6": "yoda.nochi2/PowPy6FS.yoda" \
+    , "Powheg+Pythia8 (FSR down)": "yoda.nochi2/PowPy8FSRDownAFII.yoda" \
+    , "Powheg+Pythia8 (FSR up)": "yoda.nochi2/PowPy8FSRUpAFII.yoda" \
+    , "Powheg+Pythia8 (ISR down)": "yoda.nochi2/PowPy8RadDownAFII.yoda" \
+    , "Powheg+Pythia8 (ISR up)": "yoda.nochi2/PowPy8RadUpAFII.yoda" \
+    , "aMC@NLO+Pythia8": "yoda.nochi2/aMCPy8AFII.yoda" \
+    }
+
+
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -5,8 +31,7 @@ import matplotlib.mlab as mlab
 import matplotlib.lines as lines
 import numpy as np
 import yoda as y
-from sys import stdin, stdout, argv
-
+from sys import stdout, argv
 
 
 def llh(modes, cov):
@@ -18,8 +43,9 @@ def llh(modes, cov):
 
     return f
 
+infile = open(argv[2])
 
-names = np.array(map(str.strip, stdin.readline().split(",")))
+names = np.array(map(str.strip, infile.readline().split(",")))
 binidxs = []
 for (i, n) in enumerate(names):
     if "normtruthbin" in n:
@@ -28,11 +54,8 @@ for (i, n) in enumerate(names):
 binidxs.sort()
 idxs = np.array([0] + map(lambda (x, y): y, binidxs))
 
-print("idxs: %s" % idxs)
-print("names[idxs]: %s" % names[idxs])
-
 # read in the toys
-xs_unsorted = np.loadtxt(stdin, delimiter=',')
+xs_unsorted = np.loadtxt(infile, delimiter=',')
 
 # only keep the llh and the observable
 xs_unsorted = xs_unsorted[:,idxs]
@@ -48,21 +71,18 @@ if len(xs.shape) == 1:
 # we remove the llh and the first bin since these are normalized
 xs = xs[2:]
 
-print("xs:")
-print(xs)
-
 modes = xs[:,0]
 
-print("modes:")
+print("modes of each bin of this observable:")
 print(modes)
 
 cov = np.cov(xs)
 
 
-print("absolute uncertainties")
+print("absolute uncertainty on each observable bin")
 print(np.sqrt(np.diag(cov)))
 
-print("absolute covariance:")
+print("absolute covariance between bins:")
 print(cov)
 
 modes = xs[:,0]
@@ -71,9 +91,6 @@ cov = np.cov(xs)
 
 thisllh = llh(modes, cov)
 llhs = np.array([thisllh(x) for x in xs.T])
-
-print("llhs:")
-print(llhs)
 
 tsts = -2*np.log(llhs)
 meantsts = np.mean(tsts)
@@ -85,22 +102,7 @@ nllhs = len(llhs)
 def pval(xs):
     return float(np.sum(thisllh(xs) > llhs)) / nllhs
 
-observable = argv[1]
-
-histkey = "/htop/elmujjtrue/truejets/" + observable
-
-files = \
-    { "PowPy8" : "yoda.nochi2/PowPy8FS.yoda" \
-    , "Sherpa221" : "yoda.nochi2/Sherpa221AFII.yoda" \
-    , "Powheg+Herwig7": "yoda.nochi2/PowH7AFII.yoda" \
-    , "Powheg+Pythia6": "yoda.nochi2/PowPy6FS.yoda" \
-    , "Powheg+Pythia8 (FSR down)": "yoda.nochi2/PowPy8FSRDownAFII.yoda" \
-    , "Powheg+Pythia8 (FSR up)": "yoda.nochi2/PowPy8FSRUpAFII.yoda" \
-    , "Powheg+Pythia8 (ISR down)": "yoda.nochi2/PowPy8RadDownAFII.yoda" \
-    , "Powheg+Pythia8 (ISR up)": "yoda.nochi2/PowPy8RadUpAFII.yoda" \
-    , "aMC@NLO+Pythia8": "yoda.nochi2/aMCPy8AFII.yoda" \
-    }
-
+histkey = argv[1]
 
 hs = {}
 
@@ -109,6 +111,7 @@ for (k, f) in files.iteritems():
     hs[k] = [b.area for b in h.bins[1:]]
 
 
+print("")
 print("pvalues:")
 
 for (k, h) in hs.iteritems():
