e91120995cb3142f9d8e96e4643210e6df6731a8
Chris Pollard <cspollard@gmail.com>
2020-02-15 09:20:52 +0100

    add tau decays to pythia generation

--- ext/runpythia.sh
+++ ext/runpythia.sh
@@ -1,5 +1,5 @@
 run-pythia -n 10000 \
-  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13" \
+  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13 15" \
   -c Tune:pp=19 \
   -c StringZ:rFactB=1.05 \
   -c PDF:pSet=13 \
@@ -8,7 +8,7 @@ run-pythia -n 10000 \
 # rivet --pwd -a BFRAG fifo -o A14-rb.yoda
 
 run-pythia -n 10000 \
-  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13" \
+  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13 15" \
   -c Tune:pp=19 \
   -c PDF:pSet=13 \
   -o A14.hepmc \
@@ -16,7 +16,7 @@ run-pythia -n 10000 \
 # rivet --pwd -a BFRAG fifo -o A14.yoda
 
 run-pythia -n 10000 \
-  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13" \
+  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13 15" \
   -c Tune:pp=14 \
   -c PDF:pSet=13 \
   -o Monash.hepmc \
@@ -24,7 +24,7 @@ run-pythia -n 10000 \
 # rivet --pwd -a BFRAG fifo -o Monash.yoda
 
 run-pythia -n 10000 \
-  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13" \
+  -c Top:all=on -c 24:onMode=off -c "24:onIfAny=11 13 15" \
   -c Tune:pp=0 \
   -c PDF:pSet=13 \
   -o Pythia.hepmc \
