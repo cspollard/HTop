07c0202e0866a1a96e91202e5b159c3b3838549c
Chris Pollard <cspollard@gmail.com>
2020-04-27 14:24:20 +0200

    move to correct nominal powpy8

--- ext/interpret.sh
+++ ext/interpret.sh
@@ -3,16 +3,16 @@ interpret () {
   mkdir -p unfold/interpret/$1
 
   python ext/pvalue.py /BFRAG/$1 unfold/mcmc/data/$1mcmc.dat \
-    gridYoda/Merge_410470_PhPy8.yoda:PowPy8 \
+    gridYoda/Merge_410503_PhPy8.yoda:PowPy8 \
     gridYoda/Merge_410465_aMcAtNloPy8.yoda:aMCNLOPy8 \
-    gridYoda/Merge_410503_PhPy8.yoda:PowPy8_410503 \
+    gridYoda/Merge_410470_PhPy8.yoda:PowPy8_410470 \
     gridYoda/Merge_411360_PP8_MECoff_grec.yoda:PowPy8_MECoff_grec \
     > unfold/interpret/$1/pythia.log \
     2>&1
 
 
   python ext/pvalue.py /BFRAG/$1 unfold/mcmc/data/$1mcmc.dat \
-    gridYoda/Merge_410470_PhPy8.yoda:PowPy8 \
+    gridYoda/Merge_410503_PhPy8.yoda:PowPy8 \
     gridYoda/Merge_410028_V2Up.yoda:PowPy8FSRUp \
     gridYoda/Merge_410029_V2Down.yoda:PowPy8FSRDown \
     gridYoda/Merge_411291_PP8_rb1p05.yoda:PowPy8_A14rb \
@@ -27,7 +27,7 @@ interpret () {
 
 
   python ext/pvalue.py /BFRAG/$1 unfold/mcmc/data/$1mcmc.dat \
-    gridYoda/Merge_410470_PhPy8.yoda:PowPy8 \
+    gridYoda/Merge_410503_PhPy8.yoda:PowPy8 \
     gridYoda/Merge_PH704.yoda:PowHer704 \
     gridYoda/Merge_411234_PH713.yoda:PowHer713 \
     gridYoda/Merge_PH16.yoda:PowHer716 \
@@ -36,7 +36,7 @@ interpret () {
 
 
   python ext/pvalue.py /BFRAG/$1 unfold/mcmc/data/$1mcmc.dat \
-    gridYoda/Merge_410470_PhPy8.yoda:PowPy8 \
+    gridYoda/Merge_410503_PhPy8.yoda:PowPy8 \
     gridYoda/Merge_410252_Sh221.yoda:Sherpa221 \
     gridYoda/Merge_950010_Sh228_HTprime.yoda:Sherpa228HTprime \
     gridYoda/Merge_950033_Sh228_CSSevol.yoda:Sherpa228CSSevol \
@@ -95,7 +95,7 @@ done
 rivet-mkhtml --mc-errs --pwd \
   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
   -c ext/htop.plot \
-  gridYoda/Merge_410470_PhPy8.yoda:"PowPy8" \
+  gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
   gridYoda/Merge_410465_aMcAtNloPy8.yoda:"aMCNLOPy8" \
   gridYoda/Merge_410503_PhPy8.yoda:"PowPy8_410503" \
   gridYoda/Merge_411360_PP8_MECoff_grec.yoda:"PowPy8_MECoff_grec" \
@@ -105,7 +105,7 @@ rivet-mkhtml --mc-errs --pwd \
 rivet-mkhtml --mc-errs --pwd \
   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
   -c ext/htop.plot \
-  gridYoda/Merge_410470_PhPy8.yoda:"PowPy8" \
+  gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
   gridYoda/Merge_410028_V2Up.yoda:"PowPy8FSRUp" \
   gridYoda/Merge_410029_V2Down.yoda:"PowPy8FSRDown" \
   gridYoda/Merge_411291_PP8_rb1p05.yoda:"PowPy8_A14rb" \
