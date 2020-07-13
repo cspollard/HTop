4d7c7d138ff85747d2c8bc5bdedfc05cb01208ae
Chris Pollard <cspollard@gmail.com>
2020-07-13 15:57:17 +0200

    interpret.sh only handles pvalue calculations now.

--- ext/interpret.sh
+++ ext/interpret.sh
@@ -92,60 +92,60 @@ do
 done
 
 
-rivet-mkhtml --mc-errs --pwd \
-  -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
-  -c ext/htop.plot \
-  gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
-  gridYoda/Merge_410465_aMcAtNloPy8.yoda:"aMCNLOPy8" \
-  gridYoda/Merge_410503_PhPy8.yoda:"PowPy8_410503" \
-  gridYoda/Merge_411360_PP8_MECoff_grec.yoda:"PowPy8_MECoff_grec" \
-  -o unfold/interpret/pythia.plots
+# rivet-mkhtml --mc-errs --pwd \
+#   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
+#   -c ext/htop.plot \
+#   gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
+#   gridYoda/Merge_410465_aMcAtNloPy8.yoda:"aMCNLOPy8" \
+#   gridYoda/Merge_410503_PhPy8.yoda:"PowPy8_410503" \
+#   gridYoda/Merge_411360_PP8_MECoff_grec.yoda:"PowPy8_MECoff_grec" \
+#   -o unfold/interpret/pythia.plots
 
 
-rivet-mkhtml --mc-errs --pwd \
-  -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
-  -c ext/htop.plot \
-  gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
-  gridYoda/Merge_410028_V2Up.yoda:"PowPy8FSRUp" \
-  gridYoda/Merge_410029_V2Down.yoda:"PowPy8FSRDown" \
-  gridYoda/Merge_411291_PP8_rb1p05.yoda:"PowPy8_A14rb" \
-  -o unfold/interpret/pythiaA14vars.plots \
+# rivet-mkhtml --mc-errs --pwd \
+#   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
+#   -c ext/htop.plot \
+#   gridYoda/Merge_410503_PhPy8.yoda:"PowPy8" \
+#   gridYoda/Merge_410028_V2Up.yoda:"PowPy8FSRUp" \
+#   gridYoda/Merge_410029_V2Down.yoda:"PowPy8FSRDown" \
+#   gridYoda/Merge_411291_PP8_rb1p05.yoda:"PowPy8_A14rb" \
+#   -o unfold/interpret/pythiaA14vars.plots \
 
 
-rivet-mkhtml --mc-errs --pwd \
-  -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
-  -c ext/htop.plot \
-  gridYoda/Merge_PH704.yoda:"PowHer704" \
-  gridYoda/Merge_411234_PH713.yoda:"PowHer713" \
-  gridYoda/Merge_PH16.yoda:"PowHer16" \
-  -o unfold/interpret/herwig.plots
+# rivet-mkhtml --mc-errs --pwd \
+#   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
+#   -c ext/htop.plot \
+#   gridYoda/Merge_PH704.yoda:"PowHer704" \
+#   gridYoda/Merge_411234_PH713.yoda:"PowHer713" \
+#   gridYoda/Merge_PH16.yoda:"PowHer16" \
+#   -o unfold/interpret/herwig.plots
 
 
-rivet-mkhtml --mc-errs --pwd \
-  -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
-  -c ext/htop.plot \
-  gridYoda/Merge_410252_Sh221.yoda:"Sherpa221" \
-  gridYoda/Merge_950010_Sh228_HTprime.yoda:"Sherpa228HTprime" \
-  gridYoda/Merge_950033_Sh228_CSSevol.yoda:"Sherpa228CSSevol" \
-  -o unfold/interpret/sherpa.plots
+# rivet-mkhtml --mc-errs --pwd \
+#   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
+#   -c ext/htop.plot \
+#   gridYoda/Merge_410252_Sh221.yoda:"Sherpa221" \
+#   gridYoda/Merge_950010_Sh228_HTprime.yoda:"Sherpa228HTprime" \
+#   gridYoda/Merge_950033_Sh228_CSSevol.yoda:"Sherpa228CSSevol" \
+#   -o unfold/interpret/sherpa.plots
 
 
-rivet-mkhtml --mc-errs --pwd \
-  -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
-  -c ext/htop.plot \
-  gridYoda/Merge_PP8_EvtGen.yoda:"PowPy8 + EvtGen" \
-  gridYoda/Merge_PP8_noEvtGen.yoda:"PowPy8 - EvtGen" \
-  -o unfold/interpret/evtgen.plots
+# rivet-mkhtml --mc-errs --pwd \
+#   -m "/BFRAG/(rho|zbtc|zblc|nsvtrk)$" \
+#   -c ext/htop.plot \
+#   gridYoda/Merge_PP8_EvtGen.yoda:"PowPy8 + EvtGen" \
+#   gridYoda/Merge_PP8_noEvtGen.yoda:"PowPy8 - EvtGen" \
+#   -o unfold/interpret/evtgen.plots
 
 
-for x in rb0855 rb0920 rb0970 rb1050
-do
-  plotrb $x
-done
+# for x in rb0855 rb0920 rb0970 rb1050
+# do
+#   plotrb $x
+# done
 
-for x in aS0120 aS0124 aS0127 aS0130 aS0136
-do
-  plotaS $x
-done
+# for x in aS0120 aS0124 aS0127 aS0130 aS0136
+# do
+#   plotaS $x
+# done
 
 rm -f BFRAG.yoda
